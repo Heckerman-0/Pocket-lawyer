f2639a1c68baed0ec7e67fb46d7a3e9e8fd7f430
Heckerman-0 <Noahrb4@gmail.com>
2026-06-29 16:16:33 -0400

    Initial Electron setup - hello world window

---
+++ preload.js
@@ -0,0 +1,4 @@
+﻿// We'll add functions here later to let the UI talk to Ollama safely
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Pocket Lawyer preload script loaded');
+});
